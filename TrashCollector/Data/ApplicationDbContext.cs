ea9c972a187d9feca94ead954b852b8e7d6fa336
danamurphy48 <danamurphy48@gmail.com>
2020-05-21 11:52:56 -0500

    Added override method OnModelCreating in ApplicationDbContext.

--- TrashCollector/Data/ApplicationDbContext.cs
+++ TrashCollector/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using TrashCollector.Models;
@@ -14,6 +15,18 @@ namespace TrashCollector.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityRole>()
+            .HasData(
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            }
+            );
         }
     }
 }
